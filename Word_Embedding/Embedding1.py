52ef38c33613290d07ddf5bd86c73476f3e85c99
kisomo <tmuthoka@gmail.com>
2017-12-30 02:42:54 -0600

    edited glove

--- Word_Embedding/Embedding1.py
+++ Word_Embedding/Embedding1.py
@@ -1,7 +1,7 @@
 from keras.preprocessing.text import one_hot
 from keras.preprocessing.sequence import pad_sequences
-from keras.models import Sequential 
-from keras.layers import Dense 
+from keras.models import Sequential
+from keras.layers import Dense
 from keras.layers import Flatten 
 from keras.layers.embeddings import Embedding
 
@@ -19,18 +19,15 @@ docs = ['Well done!',
 # define class labels
 labels = [1,1,1,1,1,0,0,0,0,0]
 # integer encode the documents
-
 vocab_size = 50 
 encoded_docs = [one_hot(d, vocab_size) for d in docs] 
 print(encoded_docs)
-
 # pad documents to a max length of 4 words
 max_length = 4 
 padded_docs = pad_sequences(encoded_docs, maxlen=max_length, padding='post') 
 print(padded_docs)
-
 # define the model
-model = Sequential()
+model = Sequential() 
 model.add(Embedding(vocab_size, 8, input_length=max_length)) 
 model.add(Flatten()) 
 model.add(Dense(1, activation='sigmoid'))
@@ -38,15 +35,15 @@ model.add(Dense(1, activation='sigmoid'))
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
 # summarize the model
 print(model.summary())
-
 # fit the model
 model.fit(padded_docs, labels, epochs=50, verbose=0)
 # evaluate the model
 loss, accuracy = model.evaluate(padded_docs, labels, verbose=0)
 print('Accuracy: %f' % (accuracy*100))
 
-#+++++++++++++++++++++++++++++++++++++using glove ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-print("++++++++++++++++++++++++++++++++++++ GLOVE ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+print("+++++++++++++++++++++++++++ using glove ++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
 from numpy import asarray
 from numpy import zeros 
 from keras.preprocessing.text import Tokenizer
@@ -112,4 +109,5 @@ model.fit(padded_docs, labels, epochs=50, verbose=0)
 # evaluate the model
 loss, accuracy = model.evaluate(padded_docs, labels, verbose=0)
 print('Accuracy: %f' % (accuracy*100))
+
 
