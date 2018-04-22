e97837933a9e5a388cf9aadcc8bbad75aa23b2c0
kisomo <tmuthoka@gmail.com>
2018-04-22 18:10:38 -0500

    added pre-trained glove data

--- projects.py
+++ projects.py
@@ -6,29 +6,31 @@ import matplotlib.pyplot as plt
 #https://embeddings.macheads101.com/
 
 
-##fake news datacamp
+##++++++++++++++++++++++++++++++++++++++++fake news datacamp +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+'''
+
 df = pd.read_csv("https://s3.amazonaws.com/assets.datacamp.com/blog_assets/fake_or_real_news.csv")
 print(df.shape)
 print(df.head(3))
 ##df.to_csv('fake_or_real_news.csv') 
 
-'''
-df = pd.read_csv("fake_or_real_news.csv")
+#df = pd.read_csv("fake_or_real_news.csv")
 
-print(df.shape)
-print(df.head(3))
+#print(df.shape)
+#print(df.head(3))
 
-print("\n\n")
-print(df.columns.values)
+#print("\n\n")
+#print(df.columns.values)
 
-df1 = df['Unnamed: 0.1', 'title', 'text', 'label']
+#df1 = df['Unnamed: 0.1', 'title', 'text', 'label']
 
-print(df1.shape)
-print(df1.head(3))
+#print(df1.shape)
+#print(df1.head(3))
 
-print("\n\n")
-print(df1.columns.values)
-'''
+#print("\n\n")
+#print(df1.columns.values)
+
 
 df = df.set_index("Unnamed: 0")
 print(df.head(3))
@@ -98,34 +100,97 @@ print("accuracy:  %0.3f" % score)
 cm = metrics.confusion_matrix(y_test, pred, labels=['FAKE','REAL'])
 plot_confusion_matrix(cm, classes = ['FAKE','REAL'])
 
+'''
 
 
 
 
 
 
+#++++++++++++++++++++++++++++++++++++ pre-trained glove or sefl train word2vec ++++++++++++++++++++++++++++++++++++++++++++++
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#word2vec
 #http://nadbordrozd.github.io/blog/2016/05/20/text-classification-with-word2vec/
 
 
+#wget http://nlp.stanford.edu/data/glove.6B.zip
+#unzip glove.6B.zip
+
+#and use load them up in python:
+
+import numpy as np
+
+with open("glove.6B.50d.txt", "rb") as lines:
+    w2v = {line.split()[0]: np.array(map(float, line.split()[1:]))
+           for line in lines}
+
+#or we can train a Word2Vec model from scratch with gensim:
+
+import gensim
+## let X be a list of tokenized texts (i.e. list of lists of tokens)
+#model = gensim.models.Word2Vec(X, size=100)
+#w2v = dict(zip(model.wv.index2word, model.wv.syn0))
+
+
+class MeanEmbeddingVectorizer(object):
+    def __init__(self, word2vec):
+        self.word2vec = word2vec
+        # if a text is empty we should return a vector of zeros
+        # with the same dimensionality as all the other vectors
+        self.dim = len(word2vec.itervalues().next())
+
+    def fit(self, X, y):
+        return self
+
+    def transform(self, X):
+        return np.array([
+            np.mean([self.word2vec[w] for w in words if w in self.word2vec]
+                    or [np.zeros(self.dim)], axis=0)
+            for words in X
+        ])
+
+
+
+#Let us throw in a version that uses tf-idf weighting scheme for good measure
+
+class TfidfEmbeddingVectorizer(object):
+    def __init__(self, word2vec):
+        self.word2vec = word2vec
+        self.word2weight = None
+        self.dim = len(word2vec.itervalues().next())
+
+    def fit(self, X, y):
+        tfidf = TfidfVectorizer(analyzer=lambda x: x)
+        tfidf.fit(X)
+        # if a word was never seen - it must be at least as infrequent
+        # as any of the known words - so the default idf is the max of 
+        # known idf's
+        max_idf = max(tfidf.idf_)
+        self.word2weight = defaultdict(
+            lambda: max_idf,
+            [(w, tfidf.idf_[i]) for w, i in tfidf.vocabulary_.items()])
+
+        return self
+
+    def transform(self, X):
+        return np.array([
+                np.mean([self.word2vec[w] * self.word2weight[w]
+                         for w in words if w in self.word2vec] or
+                        [np.zeros(self.dim)], axis=0)
+                for words in X
+            ])
+
+
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import ExtraTreesClassifier
+
+etree_w2v = Pipeline([
+    ("word2vec vectorizer", MeanEmbeddingVectorizer(w2v)),
+    ("extra trees", ExtraTreesClassifier(n_estimators=200))])
+etree_w2v_tfidf = Pipeline([
+    ("word2vec vectorizer", TfidfEmbeddingVectorizer(w2v)),
+    ("extra trees", ExtraTreesClassifier(n_estimators=200))])
 
 
 
@@ -137,12 +202,7 @@ plot_confusion_matrix(cm, classes = ['FAKE','REAL'])
 
 
 
-
-
-
-
-
-#organizing emails
+#+++++++++++++++++++++++++++++++++++++++++++++++++organizing emails +++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #http://www.andreykurenkov.com/writing/ai/organizing-my-emails-with-a-neural-net/
 
 
