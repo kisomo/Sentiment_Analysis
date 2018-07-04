9bcc870069ee19f19429fbead5ed1b117932107e
kisomo <tmuthoka@gmail.com>
2018-07-03 23:56:05 -0500

    cleaned the original code

--- combined_models.py
+++ combined_models.py
@@ -50,29 +50,47 @@ X, y = np.array(X), np.array(y)
 print ("total examples %s" % len(y))
 print(X.shape)
 print(y.shape)
-print(X[:1])
-print(y[:20])
+#print(X[0])
+print(len(X[0]))
+print(type(X))
+print(y[0])
+print(np.unique(y))
 
-
-
-
+'''
 with open(GLOVE_6B_50D_PATH, "rb") as lines:
     wvec = {line.split()[0].decode(encoding): np.array(line.split()[1:],dtype=np.float32)
     #w2v = {line.split()[0].decode(encoding): np.array(line.split()[1:],dtype=np.float32)
                for line in lines}
 
 print(type(wvec))
+#print(wvec.keys())
+print(len(wvec))
 
+'''
+from itertools import islice
+
+def take(n, iterable):
+    "Return first n items of the iterable as a list"
+    return list(islice(iterable, n))
+
+'''
+n_items = take(2, wvec.keys())
+#for index in wvec.keys():
+for index in n_items:
+    print(index, "==>", wvec[index])
+
+'''
 # reading glove files, this may take a while
 # we're reading line by line and only saving vectors
 # that correspond to words from our training set
 # if you wan't to play around with the vectors and have 
 # enough RAM - remove the 'if' line and load everything
 
-import struct 
-
+#import struct 
+print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 glove_small = {}
 all_words = set(w for words in X for w in words)
+print(len(all_words))
 with open(GLOVE_6B_50D_PATH, "rb") as infile:
     for line in infile:
         parts = line.split()
@@ -81,7 +99,12 @@ with open(GLOVE_6B_50D_PATH, "rb") as infile:
             nums=np.array(parts[1:], dtype=np.float32)
             glove_small[word] = nums
 
-            
+n_items = take(2, glove_small.keys())
+
+for index in n_items:
+    print(index, "==>", glove_small[index])
+
+print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")          
 glove_big = {}
 with open(GLOVE_840B_300D_PATH, "rb") as infile:
     for line in infile:
@@ -91,13 +114,41 @@ with open(GLOVE_840B_300D_PATH, "rb") as infile:
             nums=np.array(parts[1:], dtype=np.float32)
             glove_big[word] = nums
 
+n_items = take(2, glove_big.keys())
 
+for index in n_items:
+    print(index, "==>", glove_big[index])
+
+print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 ## train word2vec on all the texts - both training and test set
 ## we're not using test labels, just texts so this is fine
-model = Word2Vec(X, size=100, window=5, min_count=5, workers=2)
+#model = Word2Vec(X, size=100, window=5, min_count=5, workers=2)
+#w2v = {w: vec for w, vec in zip(model.wv.index2word, model.wv.syn0)}
+
+model = Word2Vec(sentences=X, # tokenized senteces, list of list of strings
+                 size=100,  # size of embedding vectors
+                 window = 5, # I don't know what this means
+                 workers=8, # how many threads?
+                 min_count=5, # minimum frequency per token, filtering rare words
+                 sample=0.05, # weight of downsampling common words
+                 sg = 0, # should we use skip-gram? if 0, then cbow
+                 iter=5,
+                 hs = 0
+        )
+
 w2v = {w: vec for w, vec in zip(model.wv.index2word, model.wv.syn0)}
 
-print(len(all_words))
+maneno = model[model.wv.vocab]
+
+print(maneno.shape)
+print (model.most_similar('buy'))
+print (model.most_similar('news'))
+print(type(w2v))
+
+n_items = take(2, w2v.keys())
+
+for index in n_items:
+    print(index, "==>", w2v[index])
 
 
 # start with the classics - naive bayes of the multinomial and bernoulli varieties
@@ -111,6 +162,13 @@ bern_nb_tfidf = Pipeline([("tfidf_vectorizer", TfidfVectorizer(analyzer=lambda x
 svc = Pipeline([("count_vectorizer", CountVectorizer(analyzer=lambda x: x)), ("linear svc", SVC(kernel="linear"))])
 svc_tfidf = Pipeline([("tfidf_vectorizer", TfidfVectorizer(analyzer=lambda x: x)), ("linear svc", SVC(kernel="linear"))])
 
+print(type(mult_nb))
+#for index in mult_nb.keys():
+#    print(index,mult_nb[index])
+
+mult_nb.fit(X,y)
+y1 = mult_nb.predict(X)
+print(y1)
 
 class MeanEmbeddingVectorizer(object):
     def __init__(self, word2vec):
@@ -130,7 +188,24 @@ class MeanEmbeddingVectorizer(object):
             for words in X
         ])
 
-  
+est1 = MeanEmbeddingVectorizer(glove_small)
+print(type(est1))
+print(est1.fit(X,y))
+print(est1.transform(X))
+
+print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+est2 = MeanEmbeddingVectorizer(glove_big)
+print(type(est2))
+print(est2.fit(X,y))
+print(est2.transform(X))
+
+#print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+#est3 = MeanEmbeddingVectorizer(w2v)
+#print(type(est3))
+#print(est3.fit(X,y))
+#print(est3.transform(X))
+
+
 # and a tf-idf version of the same
 class TfidfEmbeddingVectorizer(object):
     def __init__(self, word2vec):
@@ -162,6 +237,23 @@ class TfidfEmbeddingVectorizer(object):
                 for words in X
             ])
 
+
+estim1 = TfidfEmbeddingVectorizer(glove_small)
+print(type(estim1))
+print(estim1.fit(X,y))
+print(estim1.transform(X))
+
+print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+estim2 = TfidfEmbeddingVectorizer(glove_big)
+print(type(estim2))
+print(estim2.fit(X,y))
+print(estim2.transform(X))
+
+#print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+#estim3 = TfidfEmbeddingVectorizer(w2v)
+#print(type(estim3))
+#print(estim3.fit(X,y))
+#print(estim3.transform(X))
 
 
 # Extra Trees classifier is almost universally great, let's stack it with our embeddings
@@ -195,6 +287,8 @@ all_models = [
     ("glove_big_tfidf", etree_glove_big_tfidf),
 
 ]
+print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+print(cross_val_score(mult_nb,X,y,cv=3)) #.mean()
 
 
 unsorted_scores = [(name, cross_val_score(model, X, y, cv=5).mean()) for name, model in all_models]
@@ -203,11 +297,12 @@ scores = sorted(unsorted_scores, key=lambda x: -x[1])
 
 print (tabulate(scores, floatfmt=".4f", headers=("model", 'score')))
 
-
+'''
 plt.figure(figsize=(15, 6))
 sns.barplot(x=[name for name, _ in scores], y=[score for _, score in scores])
 plt.show()
 
+'''
 
 
 '''
@@ -241,35 +336,6 @@ fig.set(xlabel="labeled training examples")
 fig.set(title="R8 benchmark")
 fig.set(ylabel="accuracy")
 '''
-
-
-
-
-
-
-
-
-
-#https://www.analyticsvidhya.com/blog/2018/04/a-comprehensive-guide-to-understand-and-implement-text-classification-in-python/
-
-
-
-
-
-
-#https://github.com/cahya-wirawan/ML-Collection/blob/master/TextClassification.py
-
-
-
-
-#https://medium.com/@sabber/classifying-yelp-review-comments-using-cnn-lstm-and-pre-trained-glove-word-embeddings-part-3-53fcea9a17fa
-
-
-
-#https://www.analyticsvidhya.com/blog/2017/07/word-representations-text-classification-using-fasttext-nlp-facebook/
-
-
-
 
 
 
