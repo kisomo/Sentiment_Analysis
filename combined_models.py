6998bb3cbf8001471d18ab7d473c35345db64dc6
kisomo <tmuthoka@gmail.com>
2018-07-04 19:12:28 -0500

    added models

--- combined_models.py
+++ combined_models.py
@@ -711,8 +711,211 @@ trainDF['label'] = labels
 
 print(trainDF.shape)
 print(trainDF.head(3))
+print(np.unique(labels))
+
+# split the dataset into training and validation datasets 
+train_x, valid_x, train_y, valid_y = model_selection.train_test_split(trainDF['text'], trainDF['label'])
+
+# label encode the target variable 
+encoder = preprocessing.LabelEncoder()
+train_y = encoder.fit_transform(train_y)
+valid_y = encoder.fit_transform(valid_y)
+
+print(train_y.shape)
+print(np.unique(train_y))
+
+#------------------ countvectorizer ----------------------------------
+# create a count vectorizer object 
+count_vect = CountVectorizer(analyzer='word', token_pattern=r'\w{1,}')
+count_vect.fit(trainDF['text'])
+
+# transform the training and validation data using count vectorizer object
+xtrain_count =  count_vect.transform(train_x)
+xvalid_count =  count_vect.transform(valid_x)
 
 
+
+#------------------ tfidf word-level ----------------------------------
+#TF(t) = (Number of times term t appears in a document) / (Total number of terms in the document)
+#IDF(t) = log_e(Total number of documents / Number of documents with term t in it)
+
+# word level tf-idf
+tfidf_vect = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', max_features=5000)
+tfidf_vect.fit(trainDF['text'])
+xtrain_tfidf =  tfidf_vect.transform(train_x)
+xvalid_tfidf =  tfidf_vect.transform(valid_x)
+
+
+#------------------ tfidf n-gram level ----------------------------------
+# ngram level tf-idf 
+tfidf_vect_ngram = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', ngram_range=(2,3), max_features=5000)
+tfidf_vect_ngram.fit(trainDF['text'])
+xtrain_tfidf_ngram =  tfidf_vect_ngram.transform(train_x)
+xvalid_tfidf_ngram =  tfidf_vect_ngram.transform(valid_x)
+
+
+#------------------ tfidf character level ----------------------------------
+
+# characters level tf-idf
+tfidf_vect_ngram_chars = TfidfVectorizer(analyzer='char', token_pattern=r'\w{1,}', ngram_range=(2,3), max_features=5000)
+tfidf_vect_ngram_chars.fit(trainDF['text'])
+xtrain_tfidf_ngram_chars =  tfidf_vect_ngram_chars.transform(train_x) 
+xvalid_tfidf_ngram_chars =  tfidf_vect_ngram_chars.transform(valid_x) 
+
+
+
+#------------------ Embeddings as features ---------------------------------
+
+# load the pre-trained word-embedding vectors 
+embeddings_index = {}
+#https://fasttext.cc/docs/en/english-vectors.html
+for i, line in enumerate(open('/home/terrence/CODING/Python/MODELS/wiki-news-300d-1M.vec')):
+    values = line.split()
+    embeddings_index[values[0]] = numpy.asarray(values[1:], dtype='float32')
+
+print(type(embeddings_index))
+print(len(embeddings_index))
+
+
+# create a tokenizer 
+token = text.Tokenizer()
+token.fit_on_texts(trainDF['text'])
+word_index = token.word_index
+
+print(type(word_index))
+
+# convert text to sequence of tokens and pad them to ensure equal length vectors 
+train_seq_x = sequence.pad_sequences(token.texts_to_sequences(train_x), maxlen=70)
+valid_seq_x = sequence.pad_sequences(token.texts_to_sequences(valid_x), maxlen=70)
+
+
+# create token-embedding mapping
+embedding_matrix = numpy.zeros((len(word_index) + 1, 300))
+for word, i in word_index.items():
+    embedding_vector = embeddings_index.get(word)
+    if embedding_vector is not None:
+        embedding_matrix[i] = embedding_vector
+
+
+
+#-------------------- Text / NLP based features -----------------------------
+
+trainDF['char_count'] = trainDF['text'].apply(len)
+trainDF['word_count'] = trainDF['text'].apply(lambda x: len(x.split()))
+trainDF['word_density'] = trainDF['char_count'] / (trainDF['word_count']+1)
+trainDF['punctuation_count'] = trainDF['text'].apply(lambda x: len("".join(_ for _ in x if _ in string.punctuation))) 
+trainDF['title_word_count'] = trainDF['text'].apply(lambda x: len([wrd for wrd in x.split() if wrd.istitle()]))
+trainDF['upper_case_word_count'] = trainDF['text'].apply(lambda x: len([wrd for wrd in x.split() if wrd.isupper()]))
+
+
+pos_family = {
+    'noun' : ['NN','NNS','NNP','NNPS'],
+    'pron' : ['PRP','PRP$','WP','WP$'],
+    'verb' : ['VB','VBD','VBG','VBN','VBP','VBZ'],
+    'adj' :  ['JJ','JJR','JJS'],
+    'adv' : ['RB','RBR','RBS','WRB']
+}
+
+
+# function to check and get the part of speech tag count of a words in a given sentence
+def check_pos_tag(x, flag):
+    cnt = 0
+    try:
+        wiki = textblob.TextBlob(x)
+        for tup in wiki.tags:
+            ppo = list(tup)[1]
+            if ppo in pos_family[flag]:
+                cnt += 1
+    except:
+        pass
+    return cnt
+
+trainDF['noun_count'] = trainDF['text'].apply(lambda x: check_pos_tag(x, 'noun'))
+trainDF['verb_count'] = trainDF['text'].apply(lambda x: check_pos_tag(x, 'verb'))
+trainDF['adj_count'] = trainDF['text'].apply(lambda x: check_pos_tag(x, 'adj'))
+trainDF['adv_count'] = trainDF['text'].apply(lambda x: check_pos_tag(x, 'adv'))
+trainDF['pron_count'] = trainDF['text'].apply(lambda x: check_pos_tag(x, 'pron'))
+
+
+
+
+
+
+#---------------------- Topic Models as features -------------------------------
+
+# train a LDA Model
+lda_model = decomposition.LatentDirichletAllocation(n_components=20, learning_method='online', max_iter=20)
+X_topics = lda_model.fit_transform(xtrain_count)
+topic_word = lda_model.components_ 
+vocab = count_vect.get_feature_names()
+
+# view the topic models
+n_top_words = 10
+topic_summaries = []
+for i, topic_dist in enumerate(topic_word):
+    topic_words = numpy.array(vocab)[numpy.argsort(topic_dist)][:-(n_top_words+1):-1]
+    topic_summaries.append(' '.join(topic_words))
+
+#models --------
+
+def train_model(classifier, feature_vector_train, label, feature_vector_valid, is_neural_net=False):
+    # fit the training dataset on the classifier
+    classifier.fit(feature_vector_train, label)
+    
+    # predict the labels on validation dataset
+    predictions = classifier.predict(feature_vector_valid)
+    
+    if is_neural_net:
+        predictions = predictions.argmax(axis=-1)
+    
+    return metrics.accuracy_score(predictions, valid_y)
+
+
+#----------------- Model - Naive Bayes -------------------------
+
+# Naive Bayes on Count Vectors
+accuracy = train_model(naive_bayes.MultinomialNB(), xtrain_count, train_y, xvalid_count)
+print "NB, Count Vectors: ", accuracy
+
+# Naive Bayes on Word Level TF IDF Vectors
+accuracy = train_model(naive_bayes.MultinomialNB(), xtrain_tfidf, train_y, xvalid_tfidf)
+print "NB, WordLevel TF-IDF: ", accuracy
+
+# Naive Bayes on Ngram Level TF IDF Vectors
+accuracy = train_model(naive_bayes.MultinomialNB(), xtrain_tfidf_ngram, train_y, xvalid_tfidf_ngram)
+print "NB, N-Gram Vectors: ", accuracy
+
+# Naive Bayes on Character Level TF IDF Vectors
+accuracy = train_model(naive_bayes.MultinomialNB(), xtrain_tfidf_ngram_chars, train_y, xvalid_tfidf_ngram_chars)
+print "NB, CharLevel Vectors: ", accuracy
+#NB, Count Vectors:  0.7004
+#NB, WordLevel TF-IDF:  0.7024
+#NB, N-Gram Vectors:  0.5344
+#NB, CharLevel Vectors:  0.6872
+
+
+
+#----------------- Model - Linear Classifier -------------------------
+
+#----------------- Model - SVM -------------------------
+
+#----------------- Model - Baggin -------------------------
+
+#----------------- Model - Boosting -------------------------
+
+#----------------- Model - Shallow Neural Nets -------------------------
+
+#----------------- Model - CNN -------------------------
+
+#----------------- Model - LSTM -------------------------
+
+#----------------- Model - GRU -------------------------
+
+#----------------- Model - Bidirectional RNN -------------------------
+
+#----------------- Model - Recurrent CNN (RCNN) -------------------------
+
+#----------------- Model - Other Deep Learning -------------------------
 
 
 
