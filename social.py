7e20cfddc92dd80200c565651471a3c18a0daf65
kisomo <tmuthoka@gmail.com>
2018-07-11 21:29:46 -0500

    added twitter and nltk

---
+++ social.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+
+
+#https://marcobonzanini.com/2015/03/02/mining-twitter-data-with-python-part-1/
+
+
+
+
+
+#https://www.toptal.com/python/twitter-data-mining-using-python
+
+
+
+
+#http://adilmoujahid.com/posts/2014/07/twitter-analytics/
+
+
+
+
+#https://www.freelancer.com/community/articles/twitter-data-mining-a-guide-to-big-data-analytics-using-python
+
+
+
+
+#https://www.quickstart.com/blog/post/how-to-use-twitter-for-data-mining/
+
+
+
